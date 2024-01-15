eb315a8a242f26aff11c6d967cd40f4acd9b8dd1
YELDAMUMESH <gliscor68@gmail.com>
2024-01-15 17:34:58 +0530

    150124

--- shared_memory/shm2.c
+++ shared_memory/shm2.c
@@ -1,22 +1,20 @@
-
 #include<stdio.h>
 #include<sys/ipc.h>
 #include<sys/types.h>
 #include<sys/shm.h>
-int main() {
-    char *p;
+int main()
+{
+    char *r;
     int id;
     id=shmget(40,250,IPC_CREAT|0644);
-    if(id<0){
+    if(id<0)
+    {
         perror("shmget");
         return 0;
-
     }
-    printf("id =%d\n",id);
-    getchar();
-    p=shmat(id,0,0);
-    printf("enter the data:");
-    scanf("%s",p);
-    printf("%s",p);
+
+    printf("id = %d\n",id);
+    r=shmat(id,0,0);
+    printf("%s\n",r);
     return 0;
 }
---
+++ shared_memory/shm3.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<sys/ipc.h>
+#include<sys/types.h>
+#include<sys/shm.h>
+int main()
+{
+    char *r;
+    int id;
+    id =shmget(40,250,IPC_CREAT|0644);
+    if(id<0){
+        perror("shmget");
+        return 0;
+    }
+    printf("id=%d\n",id);
+    r=shmat(id,0,0);
+    printf("%s\n",r);
+    getchar();
+    
+}
